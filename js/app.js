0b5dbf5cf16871f039d3ccd34861a71a01a9d219
Yoshihisa Tanaka <yt.hisa@gmail.com>
2013-07-08 20:41:21 +0900

    change to show top 20 results

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 function MainCtrl($scope, $http) {
   $scope.search = function() {
     var query = encodeURIComponent($scope.query);
-    $http.get('/search/test?wt=json&q=' + query).success(function(data) {
+    $http.get('/search/test?wt=json&rows=20&q=' + query).success(function(data) {
       $scope.query = data.responseHeader.params.q;
       $scope.result = data;
       $scope.docs = {};
