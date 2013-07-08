be7052de81ed5008d13fa07c567f0dcbeb2e1179
Yoshihisa Tanaka <yt.hisa@gmail.com>
2013-07-08 20:33:59 +0900

    fetch and show actual content for each result

--- js/app.js
+++ js/app.js
@@ -6,6 +6,13 @@ function MainCtrl($scope, $http) {
     $http.get('/search/test?wt=json&q=' + query).success(function(data) {
       $scope.query = data.responseHeader.params.q;
       $scope.result = data;
+      $scope.docs = {};
+      data.response.docs.forEach(function(doc) {
+        var url = '/buckets/' + doc._yz_rb + '/keys/' + doc._yz_rk;
+        $http.get(url).success(function(data) {
+            $scope.docs[doc._yz_rk] = data;
+        });
+      })
     });
   }
 }
